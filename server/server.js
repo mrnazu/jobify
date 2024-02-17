c7b75207102c552c37325f42c95b5d7564c4fd44
mrnazu <samuelamsalu1998@gmail.com>
2024-02-17 14:26:46 -0800

    frontend & backend setup done.

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require("./app/app");
+require("dotenv").config();
+
+// setting up a PORT
+const PORT = process.env.PORT || 3000;
+
+// starting up a server
+const server = http.createServer(app);
+server.listen(PORT, console.log(`Server is running on port ${PORT}`));
