9217df70b6e63646b156fd421d3761c0f021c09a
mrnazu <samuelamsalu1998@gmail.com>
2024-02-19 08:42:48 -0800

    Added Swagger UI and connected to MongoDB.

--- server/app/app.js
+++ server/app/app.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-module.exports = app;
-
 // Routes
-const authRouter = require('../routes/AuthRoute');
-const jobRouter = require('../routes/JobsRoute');
+const authRouter = require("../routes/AuthRoute");
+const jobRouter = require("../routes/JobsRoute");
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobRouter);
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// DB
+const connectDB = require("../config/DBconnect");
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI).then(console.log('db connected'));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+module.exports = { app, start };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require("./app/app");
+const {app, start} = require("./app/app");
 require("dotenv").config();
 
 // setting up a PORT
@@ -8,3 +8,5 @@ const PORT = process.env.PORT || 3001;
 // starting up a server
 const server = http.createServer(app);
 server.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
+start();
