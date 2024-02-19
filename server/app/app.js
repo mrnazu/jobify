f738a7eebec2b286cc52cdf3e56de9670dd8b0fa
mrnazu <samuelamsalu1998@gmail.com>
2024-02-19 09:08:03 -0800

    Added security packages, error handling, documentation and authentication routes.

--- server/app/app.js
+++ server/app/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // Routes
 const authRouter = require("../routes/AuthRoute");
 const jobRouter = require("../routes/JobsRoute");
+
+// error handler
+const notFoundMiddleware = require("../middleware/notFound");
+const errorHandlerMiddleware = require("../middleware/errHandler");
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobRouter);
@@ -15,13 +33,28 @@ const swaggerDocument = YAML.load("./swagger.yaml");
 
 // DB
 const connectDB = require("../config/DBconnect");
+const authenticateUser = require("../middleware/Auth");
 const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URI).then(console.log('db connected'));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
+  try {
+    await connectDB(process.env.MONGO_URI).then(console.log("db connected"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = { app, start };
