3510c49cc9e79427417177739ab6025bdbc7275e
mrnazu <samuelamsalu1998@gmail.com>
2024-02-21 15:35:39 -0800

    #DONE w/ swagger doc

--- server/routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {signin, signup} = require('../controllers/Auth');
-
-router.post('/signup', signup);
-router.post('/signin', signin);
-
-module.exports = router;
